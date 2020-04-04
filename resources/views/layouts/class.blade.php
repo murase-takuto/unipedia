c57d0145ff030b4193df18f6bae70a8de9c36fab
takuma <Homies05.inc@gmail.com>
2020-04-04 19:53:39 +0900

    [update] スケジュールの削除機能修正
    モダールの位置をナビバーに　レスポンシブ対応可能

---
+++ resources/views/layouts/class.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app">
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="{{ route('schedules.index') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        &nbsp;
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown">
+                            <a class="btn btn-info" data-toggle="modal" data-target="#class-delete-confirm-modal" style="border-color:transparent">
+                                授業をコマから外す
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        @yield('content')
+    </div>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script>
+        jQuery(function() {
+            $('#schedule').on('click', function() {
+                $('.navs').removeClass('active');
+                $(this).addClass('active');
+            });
+
+            $('#thread').on('click', function() {
+                $('.navs').removeClass('active');
+                $(this).addClass('active');
+            });
+
+            $('#profile').on('click', function() {
+                $('.navs').removeClass('active');
+                $(this).addClass('active');
+            });
+
+            $('#question').on('click', function() {
+                $('.navs').removeClass('active');
+                $(this).addClass('active');
+            });
+        })
+    </script>
+</body>
+
+</html>
