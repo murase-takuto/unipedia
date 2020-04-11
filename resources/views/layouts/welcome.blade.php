9e269af17c33a2659fa2ca636bdb8380e9cc33bf
takuto <Homies05.inc@gmail.com>
2020-04-11 17:55:16 +0900

    [update] resolve all conflicts
    全てのコンフリクト解消

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -11,11 +11,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<<<<<<< HEAD
     <title>{{ config('app.name', 'Unipedia') }}｜大学生のための情報共有広場</title>
-=======
-    <title>{{ config('app.name', 'Laravel') }}｜大学生のための情報共有広場</title>
->>>>>>> 8745cc6... [update] welcomeページの修正
 
     <!--metaタグ-->
     <meta content="全国の大学生必須の時間割・授業情報共有アプリ。このアプリで大学生活がガラッと変わる。簡単登録ですぐに使える！過去問や試験範囲など授業について情報共有はもちろん、サークルなど学生生活の情報発信もこのアプリひとつですべて解決！" name="description">
@@ -38,9 +34,6 @@
         @yield('content')
     </div>
 
-<<<<<<< HEAD
-    <script src="{{ asset('js/app.js') }}"></script>
-=======
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
     <script>
@@ -66,7 +59,6 @@
             });
         })
     </script>
->>>>>>> 8745cc6... [update] welcomeページの修正
 </body>
 
 </html>
