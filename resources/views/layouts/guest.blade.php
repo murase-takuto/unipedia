8a0385c63be08ea2c123f316c2971b147e53aeb4
takuma <Homies05.inc@gmail.com>
2020-04-14 02:06:15 +0900

    [update] modify layouts
    titleなどがLaravelの部分の修正・ログイン画面のform上部に15px分の余白

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}｜大学生のための情報共有広場</title>
+    <title>{{ config('app.name', 'Unipedia') }}｜大学生のための情報共有広場</title>
 
     <!--metaタグ-->
     <meta content="全国の大学生必須の時間割・授業情報共有アプリ。このアプリで大学生活がガラッと変わる。簡単登録ですぐに使える！過去問や試験範囲など授業について情報共有はもちろん、サークルなど学生生活の情報発信もこのアプリひとつですべて解決！" name="description">
@@ -37,7 +37,7 @@
                 <div class="navbar-header">
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ route('schedules.index') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        {{ config('app.name', 'Unipedia') }} | 大学生のための情報共有広場
                     </a>
                 </div>
 
